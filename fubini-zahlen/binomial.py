ec05b6e251fa14b36db6c267c32c705189bac9cb
asqiir <asqiir@blaupost.de>
2021-03-12 18:12:53 +0100

    fubini: aufgabe fertig, license+readme fehlt noch

---
+++ fubini-zahlen/binomial.py
@@ -0,0 +1,15 @@
+def row_array(rows, columns, default_val):
+	return [ [default_val for j in range (0,columns)] for i in range(0,rows)] #[[default_val] * columns] * rows
+
+def generate_lookup(size):
+	A = row_array(size,size,0)
+
+	for i in range(0,size):
+		A[i][0]=1
+		A[i][i]=1
+
+	for k in range(0,size):
+		for j in range(k+1,size):
+			A[j][k] = A[j-1][k-1] + A[j-1][k]
+	
+	return A	
---
+++ fubini-zahlen/fubini.py
@@ -0,0 +1,25 @@
+#docker build -t fubini .
+#docker run fubini <natürliche Zahl>
+
+
+
+
+#Idee: Berechnung a(n)=summe_{i=1}^n (n über i) * a(n-i)
+
+
+import sys
+import binomial
+n=int(sys.argv[1]) #eingegebene Zahl n
+
+#Berechne (b über a) für a,b∈{0,..,n} -> Speicherplatzverbrauch ∈ O(n^2) ( (n+1)^2 integer == für n<=32 ist das weniger als 4 kB)
+binomiales = binomial.generate_lookup(n+1)
+
+fubini = [1]
+for k in range(1,n+1): # berechnung der k-ten fubini-zahl
+#	val = 0
+#	for i in range(1,k+1):
+#		val += binomiales[k][i] * fubini[k-i]
+#	fubini += [val]
+	fubini += [sum([binomiales[k][i] * fubini[k-i] for i in range(1,k+1)])]
+
+print(fubini)
