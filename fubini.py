61826482d9d61a98279aaa942d9968e0f2dbcacc
asqiir <asqiir@blaupost.de>
2021-03-13 20:48:34 +0100

    n, not n+1 numbers output

--- fubini.py
+++ fubini.py
@@ -12,10 +12,10 @@ import binomial
 n=int(sys.argv[1]) #eingegebene Zahl n
 
 #Berechne (b über a) für a,b∈{0,..,n} -> Speicherplatzverbrauch ∈ O(n^2) ( (n+1)^2 integer == für n<=32 ist das weniger als 4 kB)
-binomiales = binomial.generate_lookup(n+1)
+binomiales = binomial.generate_lookup(n)
 
 fubini = [1]
-for k in range(1,n+1): # berechnung der k-ten fubini-zahl
+for k in range(1,n): # berechnung der k-ten fubini-zahl
 #	val = 0
 #	for i in range(1,k+1):
 #		val += binomiales[k][i] * fubini[k-i]
